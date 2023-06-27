20caa5229d254e49d17cf15b6c5ece4a7f5b5bfa
PierreFECalvet <pfecalvet@gmail.com>
2023-06-27 21:34:02 +0200

    Corrections 	modified:   __init__.py 	new file:   api_keys.py 	modified:   setup.py

---
+++ api_keys.py
@@ -0,0 +1,19 @@
+import os
+import configparser
+
+def get_api_key():
+    config = configparser.ConfigParser()
+    if not os.path.exists('config.ini') or not config.read('config.ini') or not 'API' in config or not 'api_key' in config['API']:
+        api_key = input("Entrez votre clé API: ")
+        config['API'] = {'api_key': api_key}
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
+        return api_key
+    else:
+        return config['API']['api_key']
+    
+def main():
+    get_api_key()
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -5,24 +5,12 @@ import sys
 
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-def get_api_key():
-    config = configparser.ConfigParser()
-    if not os.path.exists('config.ini') or not config.read('config.ini') or not 'API' in config or not 'api_key' in config['API']:
-        api_key = input("Entrez votre clé API: ")
-        config['API'] = {'api_key': api_key}
-        with open('config.ini', 'w') as configfile:
-            config.write(configfile)
-        return api_key
-    else:
-        return config['API']['api_key']
     
 def main():
     with open("requirements.txt", 'r') as f:
         packages = [line.strip() for line in f]
         for package in packages:
             install(package)
-    get_api_key()
 
 if __name__ == "__main__":
     main()
