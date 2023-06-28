65dda05b6626dfed47e8af748b3b833e4debe100
PierreFECalvet <pfecalvet@gmail.com>
2023-06-28 11:14:52 +0200

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="BabbarPy",
-    version="1.0.5",
+    version="1.0.6",
     description="API routes for www.babbar.tech",
     author="Pierre Calvet",
     author_email="support@babbar.tech",
